0d8906af11675e0379648fe11342e2ebbde1fa6a
KolaJosh <ayoolak@ymail.com>
2022-03-28 11:35:07 +0200

    new

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+import logo from "../src/logo.png";
+import "./App.css";
+
+class App extends React.Component {
+  render = () => {
+    return (
+      <div className="card">
+        <div className="header">
+          <div className="logo">
+            <a href="/">
+              <img src={logo} alt="" />
+            </a>
+          </div>
+          <div className="social">
+            <p>
+              <span className="bold"> CHRISASON FIRE PROTECTION</span>
+              <br />
+              Sorry, we are doing some work on the site
+            </p>
+          </div>
+        </div>
+        <div className="content">
+          <div className="title-holder"></div>
+          <a href="info@chrisasonfire.com">
+            <div className="cta">Send us an email</div>
+          </a>
+        </div>
+        <div className="footer">
+          <p>
+            CHRISASON FIRE PROTECTION LTD Over 260,000hours expirtising in Fire
+            Engineering Since 1987 we have been providing Africa with easy
+            access to safety technology
+            <br />
+            <br />
+            Email: info@chrisasonfire.com
+            <br />
+            <br />
+            Work: +234 803 335 6183
+          </p>
+          {/*<a
+            href="https://facebook.com"
+            title="Facebook"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <IconFacebook className="icon" />
+          </a>
+          <a
+            href="https://twitter.com"
+            title="Twitter"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <IconTwitter className="icon" />
+    </a>*/}
+        </div>
+      </div>
+    );
+  };
+}
+
+export default App;
